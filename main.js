78bfe8c48ba6408fb70a56766b4d0b2939338b7e
vibhanutula <82049803+vibhanutula@users.noreply.github.com>
2021-12-01 17:54:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function preLoad() {
+
+}
+
+function setup() {
+    canvas = createCanvas(500, 350);
+    canvas.position(530, 250);
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('MyLiptastauchPiccy.png');
+}
+
+function Moustach() {
+    var Mouse = 100;
+    console.log("Moustach Filter Added");
+}
+
+function Lips() {
+    var Stick = 200;
+    console.log("Lips Filter Added");
+}
+
+function Both() {
+    var moip = 300;
+    console.log("Both Filters Added");
+}
